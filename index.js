0695cfdff1c35fed4dbbbac11045ec1936b3f849
Anirudh Sharma <shaniru@amazon.com>
2024-05-30 13:42:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const axios = require('axios');
+const { exec } = require('child_process');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -17,6 +18,19 @@ app.get('/ip/:ipAddress', async (req, res) => {
     console.error(error);
     res.status(500).send('Error fetching IP information');
   }
+});
+
+app.get('/curl-ec2/:privateIp', (req, res) => {
+  const privateIp = req.params.privateIp;
+
+  exec(`curl http://${privateIp}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      res.status(500).send('Error curling EC2 instance');
+      return;
+    }
+    res.send(`Curl response from EC2 instance with private IP ${privateIp}:\n${stdout}`);
+  });
 });
 
 app.listen(port, () => {
